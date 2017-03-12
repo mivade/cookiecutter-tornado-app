6499c960a0e53bf992923b74c56acc7421f96388
Michael V. DePalatis <mike@depalatis.net>
2017-03-12 17:11:59 -0400

    Add options and create app

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -1,3 +1,7 @@
+from tornado.options import options
+
 __author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+options.define("port", default=8008, help="HTTP port to listen on")
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app.py
@@ -1,5 +1,33 @@
+import logging
+import os.path as osp
+
+from tornado.ioloop import IOLoop
 from tornado.web import Application
+from tornado.options import options
+
+from .handlers import IndexHandler
+
+logger = logging.getLogger(__name__)
 
 
-def make_app(urls, debug=False):
-    app = Application(urls, debug=debug)
+def subdir(*paths):
+    here = osp.abspath(osp.dirname(__file__))
+    return osp.join(here, *paths)
+
+
+def make_app(debug=False):
+    app = Application(
+        [
+            (r"/", IndexHandler),
+        ],
+        static_path=subdir("static"),
+        template_path=subdir("templates"),
+        debug=debug)
+
+
+if __name__ == "__main__":
+    options.parse_command_line()
+    app = make_app(debug=True)
+    app.listen(options.port)
+    logger.info("Listening on port %d", options.port)
+    IOLoop.current().start()
